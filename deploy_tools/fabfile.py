c5d592492d5ce61930af4f1a76262ef9d1022068
Gulacsy-Toth <tompascall@gmail.com>
2018-06-02 13:30:22 +0200

    fix fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -36,7 +36,7 @@ def _create_or_update_dotenv():
     append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
-  run('.virtualenv/bin/python manage.py collectstatic --noinput')
+  run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 def _update_database():
-  run('.virtualenv/bin/python manage.py migrate --noinput')
+  run('./virtualenv/bin/python manage.py migrate --noinput')
